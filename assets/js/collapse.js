7587ae866eaec1fbe9c7fd5bb493120ba093ecc2
MarinaP-prog <marina.pavolova@gmail.com>
2025-02-15 20:28:06 +0100

    hamburger menu collapse added

---
+++ assets/js/collapse.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navItems  = document.querySelectorAll('.nav-item');
+const navbarCollapse = document.getElementById('navbarNav');
+
+navItems.forEach(item =>{
+    item.addEventListener('click', ()=>{
+        if(navbarCollapse.classList.contains('show')){
+            new bootstrap.Collapse(navbarCollapse).hide();
+        }
+    })
+})
+
+})
